7cfd3f05426491b08bb9b5cb560cf41bf9150228
Bruno Jacby <brunojck@hotmail.com>
2023-02-08 14:10:45 +0000

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import List from "./components/List"
+import AddToList from "./components/AddToList"
+
+import brunaImg from "./img/bruna.jpg"
+import beatrizImg from "./img/beatriz.jpg"
+import pipocamg from "./img/pipoca.jpg"
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+    name: "Bruna Gama",
+    url: brunaImg,
+    age: 28,
+    note: "A gatinha da festa"
+  },
+  {
+    name: "Beatriz",
+    url: beatrizImg,
+    age: 20,    
+  },
+  {
+    name: "Pipoca",
+    url: pipocamg,
+    age: 5,
+    note: "Gosta de brincar"
+  }
+])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my Party</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
